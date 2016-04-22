bbb8e32ab0da663c6f61a799321b9802750c06db
Ryan <ryangrrtt228@gmail.com>
2016-04-22 13:26:33 -0600

    implemented coreData, created Model, and made view in StoryBoard

---
+++ ShoppingList+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingList+CoreDataProperties.swift
+//  ShoppingList
+//
+//  Created by Batman on 4/22/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension ShoppingList {
+
+    @NSManaged var isComplete: NSNumber?
+    @NSManaged var item: String?
+
+}
---
+++ ShoppingList.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList.swift
+//  ShoppingList
+//
+//  Created by Batman on 4/22/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class ShoppingList: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
